0148f68c86e2cbd873eca507e86d472488f63400
juanesrp <juanesrp@gmail.com>
2025-05-28 08:11:46 -0500

    update project

--- solrquery/src/main/java/com/example/solrquery/service/CreateCopyFieldsService.java
+++
@@ -1,9 +0,0 @@
-package com.example.solrquery.service;
-
-import org.springframework.http.ResponseEntity;
-
-import com.example.solrquery.dto.CreateCopyFieldsRequest;
-
-public interface CreateCopyFieldsService {
-  ResponseEntity<?> createCopyFields(CreateCopyFieldsRequest request);
-}
